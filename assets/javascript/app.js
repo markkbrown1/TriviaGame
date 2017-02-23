7209c461585e1de25d2730c6b5cc133abc3b1069
mark <markkbrown1@gmail.com>
2017-02-22 23:53:17 -0500

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// create variables
+
+var gotRight = 0;
+var gotWrong = 0;
+var noAnswer = 0;
+var questionArray= [];
+var answerArray = [];
+
+$(document).ready(function() {
+	$(".mainArea").html(start);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
